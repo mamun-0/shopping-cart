406d5a9bde34a7b867d9b91597a42bdf66bfcb47
mamun-0 <md.abdullahalmamun200@gmail.com>
2023-06-02 11:06:13 +0600

    create store.js and store product items and insert script in index.html as a module type

---
+++ js/store.js
@@ -0,0 +1,21 @@
+const data = [
+  {
+    id: 1,
+    name: 'Mango',
+    imgUrl: `https://th.bing.com/th/id/OIP.JqfCag3pGgrs8qWwgSsk4gHaHS?pid=ImgDet&rs=1`,
+    price: 12034,
+  },
+  {
+    id: 2,
+    name: 'Orange',
+    imgUrl: `https://th.bing.com/th/id/R.84eafafc8fe196b1324d2fef2b8fdc72?rik=71vR0Jemo0F5rw&pid=ImgRaw&r=0`,
+    price: 11067,
+  },
+  {
+    id: 3,
+    name: 'Avocado',
+    imgUrl: `https://th.bing.com/th/id/R.c72d7edfe419495937dc5c02bbcd85e7?rik=hRc18WgImzh3iw&riu=http%3a%2f%2fmassageinflorida.com%2fblog%2fwp-content%2fuploads%2f2012%2f04%2fAvocado-5918425_l1.jpg&ehk=kQRbjSajWN54cFW8D1%2fm6wLdOE3Fz8mu5K7aGxN12Q0%3d&risl=&pid=ImgRaw&r=0`,
+    price: 22092,
+  },
+];
+export default data;
